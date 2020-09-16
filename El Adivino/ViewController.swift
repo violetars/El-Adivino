c1735127f20d2f54551ac30325b6179a6ea2aca7
Violeta <violetarecio@gmail.com>
2020-09-16 15:35:47 +0200

    Initial Commit

---
+++ El Adivino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  El Adivino
+//
+//  Created by Violeta Recio Sansón on 16/09/2020.
+//  Copyright © 2020 Violeta Recio Sansón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
